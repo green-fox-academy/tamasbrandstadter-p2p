48415cc1fb43437c5dde4a258c0bf8fadb4f9674
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-05-17 10:33:20 +0200

    initial commit

---
+++ src/main/java/com/greenfox/P2pchatApplication.java
@@ -0,0 +1,18 @@
+package com.greenfox;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2pchatApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2pchatApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
