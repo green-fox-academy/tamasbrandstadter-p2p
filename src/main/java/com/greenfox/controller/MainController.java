6f523ccf08848e8d3913bb4a90d277fdc836b6a1
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-05-17 16:45:17 +0200

    created update method and endpoint

--- src/main/java/com/greenfox/controller/MainController.java
+++ src/main/java/com/greenfox/controller/MainController.java
@@ -1,16 +1,18 @@
 package com.greenfox.controller;
 
+import com.greenfox.model.User;
 import com.greenfox.service.ErrorMessage;
 import com.greenfox.service.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MainController {
   @Autowired
   private UserRepository userRepository;
+  private String error;
 
   @RequestMapping("/")
   public String main() {
@@ -19,6 +21,26 @@ public class MainController {
     } else {
       return "index";
     }
+  }
+
+  @RequestMapping("/update")
+  public String update(Model model, @RequestParam("newName") String newName) {
+    if (newName.isEmpty()) {
+      error = "The username field is empty.";
+      model.addAttribute("error", error);
+      return "redirect:/";
+    } else {
+      User user = userRepository.findOne((long)1);
+      updateExecute(user, newName);
+              //((long)1).setName(newName);
+      return "redirect:/";
+    }
+  }
+
+  @PutMapping("/update/execute")
+  public void updateExecute(User user, String newName) {
+    user.setName(newName);
+    userRepository.save(user);
   }
 
   @ExceptionHandler(Exception.class)
--- src/main/java/com/greenfox/model/User.java
+++ src/main/java/com/greenfox/model/User.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 public class User {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
-  private Long id;
+  private long id;
   private String name;
 
   public User(){
