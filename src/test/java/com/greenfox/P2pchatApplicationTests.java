1744b0518e7b32b0b7542d72fb41d6bd8ad66f3f
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-05-19 09:58:43 +0200

    created first mvcmock test

--- src/test/java/com/greenfox/P2pchatApplicationTests.java
+++ src/test/java/com/greenfox/P2pchatApplicationTests.java
@@ -1,16 +1,60 @@
 package com.greenfox;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.nio.charset.Charset;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@WebAppConfiguration
+@EnableWebMvc
 public class P2pchatApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+  private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+          MediaType.APPLICATION_JSON.getSubtype(),
+          Charset.forName("utf8"));
 
+  private MockMvc mockMvc;
+
+  @Autowired
+  private WebApplicationContext webApplicationContext;
+
+  @Before
+  public void setup() throws Exception {
+    this.mockMvc = webAppContextSetup(webApplicationContext).build();
+  }
+
+  @Test
+  public void testForOk() throws Exception {
+    mockMvc.perform(post("/api/message/receive")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("{\n" +
+                    "  \"chatmessage\": {\n" +
+                    "    \"id\": 7655482,\n" +
+                    "    \"username\": \"EggDice\",\n" +
+                    "    \"text\": \"How you doin'?\",\n" +
+                    "    \"timestamp\": 1322018752992\n" +
+                    "  },\n" +
+                    "  \"client\": {\n" +
+                    "    \"id\": \"EggDice\"\n" +
+                    "  }\n" +
+                    "}"))
+            .andExpect(status().isOk())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("ok"));
+  }
 }
